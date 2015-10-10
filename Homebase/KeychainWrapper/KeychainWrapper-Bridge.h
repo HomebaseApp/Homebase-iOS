d9a420bd2dca1bf43217d5eb50221ca84925e963
Justin Oroz <justin.oroz@me.com>
2015-10-10 05:55:45 -0700

    Login to Firebase!

---
+++ Homebase/KeychainWrapper/KeychainWrapper-Bridge.h
@@ -0,0 +1,14 @@
+//
+//  KeychainWrapper-Bridge.h
+//  Homebase
+//
+//  Created by Justin Oroz on 10/10/15.
+//  Copyright © 2015 HomeBase. All rights reserved.
+//
+
+#ifndef KeychainWrapper_Bridge_h
+#define KeychainWrapper_Bridge_h
+
+#import "KeychainWrapper.h"
+
+#endif /* KeychainWrapper_Bridge_h */
--- Homebase/LogIn.swift
+++ Homebase/LogIn.swift
@@ -7,9 +7,18 @@
 //
 
 import UIKit
+import Firebase
 
 class LogIn: UIViewController {
 
+    let MyKeychainWrapper = KeychainWrapper()
+
+    @IBOutlet weak var emailField: UITextField!
+    
+    @IBOutlet weak var passwordField: UITextField!
+    
+    @IBOutlet weak var LogInButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,7 +30,79 @@ class LogIn: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    @IBAction func enterPassword(sender: AnyObject) {
+        passwordField.becomeFirstResponder()
+    }
+    
+    @IBAction func loginAction(sender: AnyObject) {
+        if (emailField.text == "" || passwordField.text == "" || emailField.text?.containsString("@") == false)  {
+            let alertView = UIAlertController(title: "Login Problem",
+                message: "Wrong username or password." as String, preferredStyle:.Alert)
+            let okAction = UIAlertAction(title: "Okay", style: .Default, handler: nil)
+            alertView.addAction(okAction)
+            self.presentViewController(alertView, animated: true, completion: nil)
+            return;
+        }
+        
+        emailField.resignFirstResponder()
+        passwordField.resignFirstResponder()
+        
+        
+        
+        let ref = Firebase(url: "https://homebasehack.firebaseio.com")
+        ref.authUser(emailField.text, password: passwordField.text,
+            withCompletionBlock: { error, authData in
+                
+                if error != nil {
+                    // There was an error logging in to this account
+                    let alertView = UIAlertController(title: "Login Problem",
+                        message: "Wrong username or password." as String, preferredStyle:.Alert)
+                    let newAccountAction = UIAlertAction(title: "Forgot Password?", style: .Default, handler: nil)
+                    alertView.addAction(newAccountAction)
+                    
+                    let okAction = UIAlertAction(title: "Okay", style: .Default, handler: nil)
+                    alertView.addAction(okAction)
 
+                    self.presentViewController(alertView, animated: true, completion: nil)
+                } else {
+                    // We are now logged in
+                    //save info in keychain
+                    
+                }
+        })
+        
+        /*
+        if checkLogin(usernameTextField.text!, password: passwordTextField.text!) {
+            performSegueWithIdentifier("dismissLogin", sender: self)
+        } else {
+            // 7.
+            let alertView = UIAlertController(title: "Login Problem",
+                message: "Wrong username or password." as String, preferredStyle:.Alert)
+            let okAction = UIAlertAction(title: "Foiled Again!", style: .Default, handler: nil)
+            alertView.addAction(okAction)
+            self.presentViewController(alertView, animated: true, completion: nil)
+        } */
+        
+    }
+
+    @IBAction func createAccount(sender: AnyObject) {
+        let ref = Firebase(url: "https://homebasehack.firebaseio.com")
+        ref.createUser(emailField.text, password: passwordField.text,
+            withValueCompletionBlock: { error, result in
+                
+                if error != nil {
+                    // There was an error creating the account
+                } else {
+                    let uid = result["uid"] as? String
+                    print("Successfully created user account with uid: \(uid)")
+                }
+        })
+    }
+    
     /*
     // MARK: - Navigation
 
