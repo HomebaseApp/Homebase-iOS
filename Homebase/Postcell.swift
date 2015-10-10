26832f8958e85d876ddfd3e9c0f4081242d31524
Justin Oroz <justin.oroz@me.com>
2015-10-10 01:39:00 -0700

    added chat button

---
+++ Homebase/Postcell.swift
@@ -0,0 +1,26 @@
+//
+//  Postcell.swift
+//  Homebase
+//
+//  Created by Justin Oroz on 10/10/15.
+//  Copyright © 2015 HomeBase. All rights reserved.
+//
+
+import UIKit
+
+class Postcell: UITableViewCell {
+
+    @IBOutlet weak var nameButton: UIButton!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
