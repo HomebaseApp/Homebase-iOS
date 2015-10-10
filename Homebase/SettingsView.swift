89df6f0192eccf17f25b97356876d1087ba422ad
Justin Oroz <justin.oroz@me.com>
2015-10-10 02:54:08 -0700

    Removed some classes, added SettingsView

---
+++ Homebase/SettingsView.swift
@@ -0,0 +1,39 @@
+//
+//  SettingsView.swift
+//  Homebase
+//
+//  Created by Justin Oroz on 10/10/15.
+//  Copyright © 2015 HomeBase. All rights reserved.
+//
+
+import UIKit
+
+class SettingsView: UIViewController {
+
+    @IBOutlet weak var tester: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        tester.setTitle("Hello World!!", forState: UIControlState.Normal)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
